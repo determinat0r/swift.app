6c4583888e9112c31ecd178353496a589ba9c7de
頼爽子 <determinat0r@yahoo.co.jp>
2018-09-25 22:17:50 +0900

    first commit

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -10,9 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var theLabel: UILabel!
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        theLabel.text = "Work Time"
+        self.view.backgroundColor = UIColor.red
+        print("workButton tapped")
+    }
+    
+    @IBAction func lestButton(_ sender: Any) {
+        theLabel.text = "Break Time"
+        self.view.backgroundColor = UIColor.blue
+        print("lestButton tapped")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
 
